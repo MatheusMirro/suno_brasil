e830ebb3fd05ee3ef675774a788ad01504774950
Gustavo Bachega <gu_bachega@hotmail.com>
2024-04-11 11:43:44 -0300

    fix: Fixing HTML tags

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -20,7 +20,7 @@ const Header: React.FC = () => {
               </div>
               <div className="md:hidden">
                 <button className="outline-none mobile-menu-button">
-                  <svg className="w-6 h-6 text-suno-dark-blue" x-show="!showMenu" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                  <svg className="w-6 h-6 text-suno-dark-blue" x-show="!showMenu" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
                     <path d="M4 6h16M4 12h16M4 18h16"></path>
                   </svg>
                 </button>
