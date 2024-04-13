d3890d081ab544accf1bbebc927211577b5b4960
Matheus Alves <mirro123@outlook.com>
2024-04-13 14:53:03 -0300

    feat: new footer images + favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <link rel="icon" href="simbolo-suno-azul.svg" sizes="any" />
     </html>
   )
 }
